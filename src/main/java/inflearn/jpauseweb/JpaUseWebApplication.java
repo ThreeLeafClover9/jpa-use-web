279219d1394bf2da9dd40338403e3fa252e51375
clover <threeleafclover9@gmail.com>
2023-06-21 23:40:31 +0900

    Build JPA use web application

---
+++ src/main/java/inflearn/jpauseweb/JpaUseWebApplication.java
@@ -0,0 +1,13 @@
+package inflearn.jpauseweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaUseWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaUseWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inflearn/jpauseweb/JpaUseWebApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.jpauseweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaUseWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
